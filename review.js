575c6fa4ed98b508a17e3d66f75634e30030e62b
Dancode-188 <danbitengo@gmail.com>
2024-09-20 17:33:42 +0300

    Practice on the use  of arrow functions for lexical bounding

--- review.js
+++ review.js
@@ -1,5 +1,5 @@
 // Convert restSum into a multi-line fat-arrow function
-const restSum = function(...otherNums) {
+const restSum = (...otherNums) => {
   let sum = otherNums.reduce((acc, el) => (acc += el));
   return sum;
 };
@@ -7,8 +7,8 @@ console.log('restSum output:');
 console.log(restSum(1, 2, 3)); // 6
 
 // Convert getFullName into a one-liner fat-arrow function with implicit return
-const getFullName = function(person) {
-  person.firstName + " " + person.lastName;
+const getFullName = person => {
+  return person.firstName + " " + person.lastName;
 };
 
 class Person {
@@ -22,7 +22,7 @@ console.log('getFullName output:');
 console.log(getFullName(new Person("Elle", "Woods"))); // Elle Woods
 
 // Convert addFive into a one-liner fat-arrow function with implicit return
-function addFive(num) {
+let addFive = num => {
   return num + 5;
 }
 
@@ -30,7 +30,7 @@ console.log('addFive output:');
 console.log(addFive(3)); // 8
 
 // Convert coupon and the function it returns into fat-arrow functions
-function coupon(discount) {
+let coupon = discount => {
   return function(prices) {
     return prices.map((price) => (price -= price * discount));
   };
